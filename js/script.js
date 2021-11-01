519a41a57c3e2512cd6e672487a8f406d7f56d21
Edwike12 <nyaunchoedwike@gmail.com>
2021-11-01 15:26:14 +0300

    add all changes

--- js/script.js
+++ js/script.js
@@ -2,3 +2,20 @@ function getSizeValue(){
     var selectedValue = document.getElementById("size").value;
     return parseInt (selectedValue);
 }
+function getSizeValue(){
+    var selectedType = document.getElementById("type").value;
+    return parseInt (selectedType);
+}
+function getSizeValue(){
+    var selectedCrust = document.getElementById("crust").value;
+    return parseInt (selectedCrust);
+}
+function getSizeValue(){
+    var selectedToppings = document.getElementById("toppings").value;
+    return parseInt (selectedToppings);
+}
+
+function getTotalAmount(){
+    var totalAmount = (getSizeValue() + getCrust() + getToppings()); 
+    alert("You have Ordered" + getQuantity("")  +  " pizza."  +  ""  +  " The Total Amount is kshs "  +  (totalAmount)  +  ""  +  " Thank you for your order welcome again.");
+}
