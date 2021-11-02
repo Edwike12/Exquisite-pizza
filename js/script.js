9f984590c089416e68a26d416fe0a6bf5d08f751
Edwike12 <nyaunchoedwike@gmail.com>
2021-11-02 06:57:06 +0300

    change in index.html

--- js/script.js
+++ js/script.js
@@ -2,20 +2,24 @@ function getSizeValue(){
     var selectedValue = document.getElementById("size").value;
     return parseInt (selectedValue);
 }
-function getSizeValue(){
+function getType(){
     var selectedType = document.getElementById("type").value;
     return parseInt (selectedType);
 }
-function getSizeValue(){
+function getCrust(){
     var selectedCrust = document.getElementById("crust").value;
     return parseInt (selectedCrust);
 }
-function getSizeValue(){
+function getToppings(){
     var selectedToppings = document.getElementById("toppings").value;
     return parseInt (selectedToppings);
 }
 
 function getTotalAmount(){
     var totalAmount = (getSizeValue() + getCrust() + getToppings()); 
-    alert("You have Ordered" + getQuantity("")  +  " pizza."  +  ""  +  " The Total Amount is kshs "  +  (totalAmount)  +  ""  +  " Thank you for your order welcome again.");
+    alert("You have Ordered" + getType("")  +  " pizza."  +  ""  +  " The Total Amount is kshs "  +  (totalAmount)  +  ""  +  " Thank you for your order welcome again.");
+    prompt("enter your location")
+    alert("You delivery fee is 150/=")
+    alert("You order will be delivered shortly")
+    alert("Continue shoppping with us each and every time .")
 }
